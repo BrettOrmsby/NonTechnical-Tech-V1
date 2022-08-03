3ac7debbc8361127b3950d6cce20063fca524d30
BrettOrmsby <brettwormsby@outlook.com>
2022-08-03 10:51:27 -0400

    Modified app and home components to work

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
+  <Navbar/>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
+  <Footer/>
 </template>
+
+<script>
+import NavBar from "@/components/Navbar.vue"
+import Footer from "@/components/Footer.vue"
+import Navbar from "@/components/Navbar.vue"
+export default {
+  components: {
+    NavBar,
+    Footer,
+    Navbar
+}
+}
+</script>
 
 <style>
   @import './assets/styles/style.css';
