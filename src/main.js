774e5942aea985b8c2ee09e2c23fe436dcd8ecd7
BrettOrmsby <brettwormsby@outlook.com>
2022-09-05 12:02:30 -0400

    Removed unused packeges

--- src/main.js
+++ src/main.js
@@ -3,15 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 
 import VueFeather from "vue-feather";
-import VueSupabase from "vue-supabase";
 
 const app = createApp(App);
-
-app.use(VueSupabase, {
-  supabaseUrl: process.env.VUE_APP_SUPABASE_URL,
-  supabaseKey: process.env.VUE_APP_SUPABASE_ANON,
-  supabaseOptions: {},
-});
 
 app.component(VueFeather.name, VueFeather);
 
